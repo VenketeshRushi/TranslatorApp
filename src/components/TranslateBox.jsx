304d675c4bbf8bfa22c9bed3804f81e2430a3d70
Raman <rushivenketesh@gmail.com>
2022-09-24 17:37:34 +0530

    done

--- src/components/TranslateBox.jsx
+++ src/components/TranslateBox.jsx
@@ -7,98 +7,113 @@ import { AiFillCopy } from "react-icons/ai";
 import { MdClear } from "react-icons/md";
 import { Animation } from "./Animation";
 
-
 export const TranslateBox = () => {
+  const [q, setQ] = useState("");
+  const [source, setSource] = useState("");
+  const [target, setTarget] = useState("");
+  const [output, setOutput] = useState("");
 
-    const [q, setQ] = useState("");
-    const [source, setSource] = useState("");
-    const [target, setTarget] = useState("");
-    const [output, setOutput] = useState("");
+  const handleSelectChange = ({ target: { value, id } }) => {
+    id === "source" && setSource(value);
+    id === "target" && setTarget(value);
+  };
 
-    const handleSelectChange = ({ target: { value, id } }) => {
-        id === "source" && setSource(value);
-        id === "target" && setTarget(value);
+  const handleGetRequest = async () => {
+    if (q.length < 1) {
+      setOutput("");
+      return false;
     }
-
-    const handleGetRequest = async () => {
-        if (q.length < 1) {
-            setOutput("");
-            return false;
-        };
-        if (source === "" || target === "") {
-            return error("Please select language");
-        }
-        try {
-            let res = await axios.post("", { q, source, target, format: "text" });
-            res = res.data.translatedText;
-            setOutput(res);
-        } catch (err) {
-            console.log(err);
-        }
+    if (source === "" || target === "") {
+      return error("Please select language");
     }
-
-    const copyToClipboard = (text) => {
-        copy(text);
-        success("Copied to clipboard!")
+    try {
+      let res = await axios.post("", {
+        q,
+        source,
+        target,
+        format: "text",
+      });
+      res = res.data.translatedText;
+      setOutput(res);
+    } catch (err) {
+      console.log(err);
     }
+  };
 
-    const resetText = () => {
-        if (q === "" && output === "") {
-            error("Textbox is already empty!")
-        } else {
-            success("Text removed!")
-            setQ("");
-            setOutput("");
-        }
+  const copyToClipboard = (text) => {
+    copy(text);
+    success("Copied to clipboard!");
+  };
+
+  const resetText = () => {
+    if (q === "" && output === "") {
+      error("Textbox is already empty!");
+    } else {
+      success("Text removed!");
+      setQ("");
+      setOutput("");
     }
- 
-//Debounce Function
-    useEffect(() => {
+  };
 
-        let timerID = setTimeout(() => {
-            handleGetRequest();
-        }, 1000);
+  //Debounce Function
+  useEffect(() => {
+    let timerID = setTimeout(() => {
+      handleGetRequest();
+    }, 1000);
 
-        return () => {
-            clearTimeout(timerID);
-        }
+    return () => {
+      clearTimeout(timerID);
+    };
+  }, [q]);
 
-    }, [q]);
+  return (
+    <>
+      <div className="mainBox">
+        <div>
+          <SelectBox id={"source"} select={handleSelectChange} />
+          <div className="box">
+            <textarea
+              onChange={(e) => {
+                setQ(e.target.value);
+              }}
+              value={q}
+              className="outputResult"
+            ></textarea>
+          </div>
+          <div className="iconBox">
+            <p>{q.length}/250</p>
+            <AiFillCopy
+              onClick={() => {
+                copyToClipboard(q);
+              }}
+              className="icon"
+            />
+            <MdClear onClick={resetText} className="icon" />
+          </div>
+        </div>
 
+        <div>
+          <SelectBox id={"target"} select={handleSelectChange} />
+          <div className="outputResult box">
+            <p id="output">{output}</p>
+          </div>
+          <div className="iconBox">
+            <p>{output.length}/250</p>
+            <AiFillCopy
+              onClick={() => {
+                copyToClipboard(output);
+              }}
+              className="icon"
+            />
+          </div>
+        </div>
+      </div>
 
-    return (
-        <>
-            <div className="mainBox">
-                <div>
-                    <SelectBox id={'source'} select={handleSelectChange} />
-                    <div className="box">
-                        <textarea onChange={(e) => { setQ(e.target.value) }} value={q} className="outputResult"></textarea>
-                    </div>
-                    <div className="iconBox">
-                        <p>{q.length}/250</p>
-                        <AiFillCopy onClick={() => { copyToClipboard(q) }} className="icon" />
-                        <MdClear onClick={resetText} className="icon" />
-                    </div>
-                </div>
+      <Animation />
 
-                <div>
-                    <SelectBox id={'target'} select={handleSelectChange} />
-                    <div className="outputResult box">
-                        <p id="output">{output}</p>
-                    </div>
-                    <div className="iconBox">
-                        <p>{output.length}/250</p>
-                        <AiFillCopy onClick={() => { copyToClipboard(output) }} className="icon" />
-                    </div>
-                </div>
-            </div>
-
-            <Animation />
-
-            <div className="tagLine">
-                <p id="madeByVenketsh">Made with ❤️ by Venketesh Rushi</p>
-            </div>
-        </>
-    );
+      <div className="tagLine">
+        <p id="madeByVenketsh">Made with ❤️ by Venketesh Rushi</p>
+      </div>
+    </>
+  );
 };
-
--- src/index.js
+++ src/index.js
@@ -1,14 +1,14 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
-import axios from 'axios';
+import React from "react";
+import ReactDOM from "react-dom/client";
+import "./index.css";
+import App from "./App";
+import reportWebVitals from "./reportWebVitals";
+import axios from "axios";
+import "./index.css";
 
 axios.defaults.baseURL = "https://libretranslate.de/translate";
 
-
-const root = ReactDOM.createRoot(document.getElementById('root'));
+const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
     <App />
